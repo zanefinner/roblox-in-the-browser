b70f87f047d375b01b23bcb9e300492b3e268547
zane <zanefinnergm@gmail.com>
2023-11-12 13:01:53 -0500

    valid socket connection

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,62 @@
 package main
 
-import "github.com/gorilla/websocket"
+import (
+	"fmt"
+	"net/http"
+	"sync"
 
-//init
+	"github.com/gorilla/websocket"
+)
+
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
 }
 
-func main() {
+var clients = make(map[string]*websocket.Conn)
+var clientsMutex sync.Mutex
 
+func main() {
+	// Socket endpoint
+	http.HandleFunc("/echo", func(resp http.ResponseWriter, req *http.Request) {
+		// Config init
+		conn, err := upgrader.Upgrade(resp, req, nil)
+		if err != nil {
+			fmt.Println("Error upgrading to WebSocket:", err)
+			return
+		}
+
+		// Lock the mutex before modifying the clients map
+		clientsMutex.Lock()
+		clients[conn.RemoteAddr().String()] = conn
+		clientsMutex.Unlock()
+
+		// Run forever
+		for {
+			msgType, msg, err := conn.ReadMessage()
+			if err != nil {
+				// Remove the connection from the clients map on error
+				clientsMutex.Lock()
+				delete(clients, conn.RemoteAddr().String())
+				clientsMutex.Unlock()
+				return
+			}
+
+			fmt.Printf("%s send: %s\n", conn.RemoteAddr(), string(msg))
+
+			// Lock the mutex before iterating over the clients map
+			clientsMutex.Lock()
+			for _, client := range clients {
+				if err = client.WriteMessage(msgType, msg); err != nil {
+					// Handle error writing to client if needed
+				}
+			}
+			clientsMutex.Unlock()
+		}
+	})
+
+	http.ListenAndServe(":8000", nil)
 }
